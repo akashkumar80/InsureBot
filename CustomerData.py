4176a7a959315c251f61cc2cb76b37e6cd204400
Akash Kumar <aakashkumar80498@gmail.com>
2025-07-13 22:48:52 +0530

    Add Customer Detail

---
+++ CustomerData.py
@@ -0,0 +1,8 @@
+policy_holder_name = "Akash Kumar"
+policy_number = "21201"
+product_name = "InsureBot"
+policy_start_date = "21 march 2025"
+total_premium_paid = "0"
+premium_due_date = "21 March 2025"
+sum_assured = "100"
+fund_value = "one lakh"
